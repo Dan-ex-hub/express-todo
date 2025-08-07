202a97ac15fe17b3dd97950a7a5162d0c04eff18
Code-with-Danie <danrose200@student.sfit.ac.in>
2025-08-07 20:21:52 +0530

    updated

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const app = express();
 
-var app = express();
-app.set("view engine", "ejs");   
-app.use(express.static('public')); 
-app.use(express.urlencoded({extended:true})); 
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method')); 
 
 let items = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     const filter = req.query.priority || "all";
     let filteritems = items;
-    if (filter !== "all") {     //copied from a source
+    if (filter !== "all") {
         filteritems = items.filter(item => item.priority === filter);
     }
     res.render("list", { ejes: filteritems, filter: filter });
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     const itemname = req.body.ele1;
     const priority = req.body.priority;
     items.push({ name: itemname, priority: priority });
     res.redirect("/");
 });
 
-app.post("/delete", function(req, res) {
-    const index = req.body.index;
+app.delete("/delete/:index", function (req, res) {
+    const index = parseInt(req.params.index);
     if (index >= 0) {
-        items.splice(index, 1);//poping didnt work so used splice
+        items.splice(index, 1);
     }
     res.redirect("/");
 });
 
-app.get("/edit/:itemindex", function(req, res) {
+app.get("/edit/:itemindex", function (req, res) {
     const indexs = parseInt(req.params.itemindex);
-    if (indexs >= 0) {
+    if (indexs >= 0 ) {
         res.render("edit", { ogitem: items[indexs], itemindex: indexs });
     } else {
         res.redirect("/");
     }
 });
 
-app.post("/edit", function(req, res) {
-    const itemindex = parseInt(req.body.index);
+app.put("/edit/:index", function (req, res) {
+    const itemindex = parseInt(req.params.index);
     const newitem = req.body.item;
     const newpriority = req.body.priority;
-    if (itemindex >= 0 && newitem) {
+    if (itemindex >= 0 &&  newitem) {
         items[itemindex] = { name: newitem, priority: newpriority };
     }
     res.redirect("/");
 });
 
-app.listen(5000,function(){
+app.listen(5000, function () {
     console.log("Server started on port 5000");
 });
